807f65ba6838a4ee26992f226269570e1181e085
liton61 <litonofficial61@gmail.com>
2023-11-06 16:05:02 +0600

    get method added for each single job

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,6 +38,14 @@ async function run() {
         // get method
         app.get('/jobs', async (req, res) => {
             const result = await jobsCollection.find().toArray();
+            res.send(result);
+        })
+
+        // get method
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await jobsCollection.findOne(query);
             res.send(result);
         })
 
